be44238438e9d22a4da302d756b172748a745d92
sanyuktanirmal17 <sanyuktanirmal17@gmail.com>
2021-05-14 23:32:04 +0530

    Added: Daily emp  Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,12 +1,25 @@
-#!/bin/bash
+#!/bin/bash -x
 
-empCheck=$((RANDOM%2))
-isPresent=1
+empRatePerHr=20
+isFullTime=1
+isFullTime=2
+numOfWorkingDays=20
 
-if [ $empCheck -eq $isPresent ]
-then 
-	echo Employee is Present
-else 	
-	echo Employee is absent
-fi
+for ((day=1; day<=$numOfWorkingDays; day++))
+do
+   empCheck=$((RANDOM%3))
+   case $empCheck in
+   $isFullTime)
+      empHrs=8
+   ;;
+   $isPartTime)
+      empHrs=4
+   ;;
+   *)
+      empHrs=0
+   ;;
+esac
+totalEmpHrs=$(($totalEmpHrs+$empHrs))
+done
+wage=$(($empRatePerHr*$totalEmpHrs))
 
